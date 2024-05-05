b0eae66ebb05655656ae6da8214afe9457bd9e4c
Camilo Serrano <camilo.serrano@bybeeconcept.com>
2024-05-05 01:40:34 -0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const router = Router();
+ 
+router.get('/', (req, res) => {    
+    res.json(
+        {
+            "Title": "Test DevSecOps Camilo Serrano......"
+        }
+    );
+})
+ 
+module.exports = router;
+
+
+
+//Fpf93Bauo7bP8jxn469y
